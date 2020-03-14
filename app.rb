49df497ae7cc98def0957748ff1cab1f3f333068
James Benjamin <james.abenj@gmail.com>
2020-03-14 20:01:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class App < Sinatra::Base
   end
 
   get '/new' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
   end
 
 
